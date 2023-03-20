c0aad6c1388130e70f5d8ef1c976679ce47ebdd8
muzaslivka <ms.dmitrieffa@gmail.com>
2023-03-20 19:06:42 +0300

    methods actracted

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -39,14 +41,7 @@ public class Main {
                 }
                 switch (numberMenu) {
                     case 1:
-                        System.out.println("Введите названия продукта, который хотите добавить: ");
-                        String newProduct = sc.nextLine();
-                        if (listProducts.contains(newProduct)){
-                            System.out.println("Такой продукт уже есть в списке");
-                        } else {
-                            listProducts.add(newProduct);
-                            System.out.println("Продукт добавлен с список. Итого продуктов в с списке: " + listProducts.size());
-                        }
+                        addProduct(listProducts, sc);
                         System.out.println();
                         break;
                     case 2:
@@ -63,29 +58,7 @@ public class Main {
                             System.out.println("Список продуктов не заполнен!");
                         } else {
                             while (true) {
-                                System.out.println("Какой продукт хотите удалить? Введите номер продукта или его название: ");
-                                showListProduct(listProducts);
-                                String inputDelete = sc.nextLine();
-                                int numberDeleteProduct;
-                                try {
-                                    numberDeleteProduct = Integer.parseInt(inputDelete);
-                                    if (numberDeleteProduct > 0 && numberDeleteProduct <= listProducts.size()) {
-                                        System.out.println("Продукт " + listProducts.get(numberDeleteProduct - 1) + " удален. Список продуктов: ");
-                                        listProducts.remove(numberDeleteProduct - 1);
-                                        showListProduct(listProducts);
-                                        break;
-                                    } else {
-                                        System.out.println("Продукта с таким номером нет в списке. Попробуйте еще раз");
-                                    }
-                                } catch (NumberFormatException exception) {
-                                    if (listProducts.remove(inputDelete)) {
-                                        System.out.println("Продукт " + inputDelete + " удален. Список продуктов: ");
-                                        showListProduct(listProducts);
-                                        break;
-                                    } else {
-                                        System.out.println("Продукт не найдет. Попробуйте еще раз");
-                                    }
-                                }
+                                if (deleteProduct(listProducts, sc)) break;
                             }
                         }
                         System.out.println();
@@ -94,20 +67,7 @@ public class Main {
                         if (listProducts.isEmpty()) {
                             System.out.println("Список продуктов не заполнен!");
                         } else {
-                            System.out.println("Введите текст для поиска:");
-                            String search = sc.nextLine();
-                            String lowerSearch = search.toLowerCase();
-                            int countSearch = 0;
-                            for (int i = 0; i < listProducts.size(); i++) {
-                                String lowerProduct = listProducts.get(i).toLowerCase();
-                                if (lowerProduct.contains(lowerSearch)) {
-                                    System.out.println((i + 1) + ". " + listProducts.get(i));
-                                    countSearch++;
-                                }
-                            }
-                            if (countSearch == 0) {
-                                System.out.println("Такого продукта нет в списке");
-                            }
+                            searchProduct(listProducts, sc);
                         }
                         System.out.println();
                         break;
@@ -116,6 +76,61 @@ public class Main {
                         System.out.println();
                 }
             }
+        }
+    }
+
+    private static void searchProduct(List<String> listProducts, Scanner sc) {
+        System.out.println("Введите текст для поиска:");
+        String search = sc.nextLine();
+        String lowerSearch = search.toLowerCase();
+        int countSearch = 0;
+        for (int i = 0; i < listProducts.size(); i++) {
+            String lowerProduct = listProducts.get(i).toLowerCase();
+            if (lowerProduct.contains(lowerSearch)) {
+                System.out.println((i + 1) + ". " + listProducts.get(i));
+                countSearch++;
+            }
+        }
+        if (countSearch == 0) {
+            System.out.println("Такого продукта нет в списке");
+        }
+    }
+
+    private static boolean deleteProduct(List<String> listProducts, Scanner sc) {
+        System.out.println("Какой продукт хотите удалить? Введите номер продукта или его название: ");
+        showListProduct(listProducts);
+        String inputDelete = sc.nextLine();
+        int numberDeleteProduct;
+        try {
+            numberDeleteProduct = Integer.parseInt(inputDelete);
+            if (numberDeleteProduct > 0 && numberDeleteProduct <= listProducts.size()) {
+                System.out.println("Продукт " + listProducts.get(numberDeleteProduct - 1) + " удален. Список продуктов: ");
+                listProducts.remove(numberDeleteProduct - 1);
+                showListProduct(listProducts);
+                return true;
+            } else {
+                System.out.println("Продукта с таким номером нет в списке. Попробуйте еще раз");
+            }
+        } catch (NumberFormatException exception) {
+            if (listProducts.remove(inputDelete)) {
+                System.out.println("Продукт " + inputDelete + " удален. Список продуктов: ");
+                showListProduct(listProducts);
+                return true;
+            } else {
+                System.out.println("Продукт не найдет. Попробуйте еще раз");
+            }
+        }
+        return false;
+    }
+
+    private static void addProduct(List<String> listProducts, Scanner sc) {
+        System.out.println("Введите названия продукта, который хотите добавить: ");
+        String newProduct = sc.nextLine();
+        if (listProducts.contains(newProduct)) {
+            System.out.println("Такой продукт уже есть в списке");
+        } else {
+            listProducts.add(newProduct);
+            System.out.println("Продукт добавлен с список. Итого продуктов в с списке: " + listProducts.size());
         }
     }
 }
